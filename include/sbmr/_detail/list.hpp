81f8dac2f57f8c8eee7e4bf041078ae844b506b7
doodspav <doodspav@gmail.com>
2022-03-07 23:41:34 +0000

    Add base modification functions to list
    - emplace
    - emplace_front
    - emplace_back
    - erase
    - clear
    
    - also add destructor
    - now can be used in a constexpr context
    
    - node now uses a union to hold value as an uninitialized member
    - this is so that it can be constructed separately to the node

--- include/sbmr/_detail/list.hpp
+++ include/sbmr/_detail/list.hpp
@@ -15,29 +15,43 @@
 namespace sbmr::_detail {
 
 
+    // union with single uninitialized field
+    // which must be manually constructed and destroyed
+
+    template <class T>
+        requires std::is_object_v<T>
+    union _list_uninit
+    {
+        // default constructible and destructible
+        constexpr _list_uninit()  noexcept = default;
+        constexpr ~_list_uninit() noexcept = default;
+
+        // holder is not copyable or movable
+        _list_uninit(const _list_uninit&) = delete;
+        _list_uninit(_list_uninit&&)      = delete;
+
+        // holder is not copy or move assignable
+        auto& operator=(const _list_uninit&) = delete;
+        auto& operator=(_list_uninit&&)      = delete;
+
+        // uninitialized field
+        [[no_unique_address]] T value;
+    };
+
+
     // simple doubly linked node
     // constructors are present because std::construct_at doesn't seem to work
     //   with aggregate types very well
 
     template <class T>
+        requires std::is_object_v<T>
     struct _list_node
     {
-        // constructor to make allocator_traits::construct happy
-        constexpr explicit _list_node(const T& value)
-            noexcept(std::is_nothrow_copy_constructible_v<T>)
-            requires std::copyable<T>
-            : value{value}
-        {}
-
-        // ditto ^^
-        constexpr explicit _list_node(T&& value)
-            noexcept(std::is_nothrow_move_constructible_v<T>)
-            requires std::movable<T>
-            : value{std::move(value)}
-        {}
+        // default constructor (all other constructors are deleted)
+        constexpr _list_node() noexcept = default;
 
         // data members
-        T value;
+        _list_uninit<T> holder;
         _list_node *next;
         _list_node *prev;
     };
@@ -52,6 +66,10 @@ namespace sbmr::_detail {
         requires std::is_object_v<T>  // must be referencable
     class _list_iterator_crtp
     {
+        // we need to friend ourselves for templated comparison operators
+        template <class T_, class Derived_, bool is_const_>
+        friend class _list_iterator_crtp;
+
     public:
 
         // member types
@@ -142,7 +160,7 @@ namespace sbmr::_detail {
         {
             SBMR_ASSERT_CONSTEXPR(m_node != nullptr);
 
-            return m_node->value;
+            return m_node->holder.value;
         }
 
         // pointer member access
@@ -152,13 +170,14 @@ namespace sbmr::_detail {
         {
             SBMR_ASSERT_CONSTEXPR(m_node != nullptr);
 
-            return std::addressof(m_node->value);
+            return std::addressof(m_node->holder.value);
         }
 
         // equality comparison with self
         // compiler will auto-generate operator!=
+        template <class D, bool is_c>
         [[nodiscard]] constexpr bool
-        operator==(const Derived& other) const noexcept
+        operator==(const _list_iterator_crtp<T, D, is_c>& other) const noexcept
         {
             return m_node == other.m_node;
         }
@@ -240,6 +259,47 @@ namespace sbmr::_detail {
         size_type  m_size = 0;
         [[no_unique_address]] rebind_allocator_type m_alloc;
 
+        // allocate and construct node with value from args
+        template <class... Args>
+            requires std::constructible_from<value_type, Args...>
+        [[nodiscard]] constexpr node_type *
+        new_node(Args&&... args)
+            noexcept(std::is_nothrow_constructible_v<value_type, Args...> &&
+                 noexcept(m_alloc.allocate(1)))
+        {
+            // no need to catch if allocate throws
+            auto *node = rebind_allocator_traits::allocate(m_alloc, 1);
+
+            // node constructor is noexcept
+            static_assert(std::is_nothrow_constructible_v<node_type>);
+            rebind_allocator_traits::construct(m_alloc, node);
+
+            // attempt to construct T (value_type)
+            try {
+                auto *addr = std::addressof(node->holder.value);
+                std::construct_at(addr, std::forward<Args>(args)...);
+            }
+            catch (...) {
+                rebind_allocator_traits::destroy(m_alloc, node);
+                rebind_allocator_traits::deallocate(m_alloc, node, 1);
+                throw;
+            }
+
+            // success
+            return node;
+        }
+
+        // destroy and de-allocate a node
+        constexpr void
+        delete_node(node_type* node) noexcept
+        {
+            SBMR_ASSERT_CONSTEXPR(node != nullptr);
+
+            std::destroy_at(std::addressof(node->holder.value));
+            rebind_allocator_traits::destroy(m_alloc, node);
+            rebind_allocator_traits::deallocate(m_alloc, node, 1);
+        }
+
     public:
 
         // Constructors - labelled according to std::list::list in cppreference
@@ -256,6 +316,12 @@ namespace sbmr::_detail {
             requires std::constructible_from<rebind_allocator_type, const Alloc&>
             : m_alloc{alloc}
         {}
+
+        // destructor
+        constexpr ~list() noexcept
+        {
+            clear();
+        }
 
 
         // Capacity
@@ -284,36 +350,44 @@ namespace sbmr::_detail {
 
         // Element access
 
+        // returns a reference to the head element
+        // pre-conditions: !empty()
         [[nodiscard]] constexpr reference
         front() noexcept
         {
             SBMR_ASSERT_CONSTEXPR(m_head != nullptr);
 
-            return m_head->value;
+            return m_head->holder.value;
         }
 
+        // returns a const_reference to the head element
+        // pre-conditions: !empty()
         [[nodiscard]] constexpr const_reference
         front() const noexcept
         {
             SBMR_ASSERT_CONSTEXPR(m_head != nullptr);
 
-            return m_head->value;
+            return m_head->holder.value;
         }
 
+        // returns a reference to the tail element
+        // pre-conditions: !empty()
         [[nodiscard]] constexpr reference
         back() noexcept
         {
             SBMR_ASSERT_CONSTEXPR(m_tail != nullptr);
 
-            return m_tail->value;
+            return m_tail->holder.value;
         }
 
+        // returns a const_reference to the tail element
+        // pre-conditions: !empty()
         [[nodiscard]] constexpr const_reference
         back() const noexcept
         {
             SBMR_ASSERT_CONSTEXPR(m_tail != nullptr);
 
-            return m_tail->value;
+            return m_tail->holder.value;
         }
 
 
@@ -359,6 +433,129 @@ namespace sbmr::_detail {
         cend() const noexcept
         {
             return const_iterator{};
+        }
+
+
+        // Modifiers
+
+        // constructs a new element in place directly before pos
+        // returns an iterator to the new element
+        template <class... Args>
+            requires std::constructible_from<value_type, Args...>
+        constexpr iterator
+        emplace(const_iterator pos, Args&&... args)
+            noexcept(noexcept(new_node(std::forward<Args>(args)...)))
+        {
+            // valid as long as we own it since emplace isn't const
+            auto* pos_node = const_cast<node_type *>(pos.m_node);
+
+            // construct our new node
+            auto *node = new_node(std::forward<Args>(args)...);
+
+            // insert as first element
+            if (pos == cend() && m_size == 0)
+            {
+                m_head = node;
+                m_tail = node;
+            }
+
+                // insert at begin (and size is not 0)
+            else if (pos == cbegin())
+            {
+                node->next = m_head;
+                m_head->prev = node;
+                m_head = node;
+            }
+
+                // insert at end (and size is not 0)
+            else if (pos == cend())
+            {
+                node->prev = m_tail;
+                m_tail->next = node;
+                m_tail = node;
+            }
+
+                // insert in middle (before pos), with elements on both sides
+            else
+            {
+                node->next = pos_node;
+                node->prev = pos_node->prev;
+                pos_node->prev = node;
+            }
+
+            // modify size to reflect changes
+            ++m_size;
+
+            // return iterator to newly created element
+            return iterator{node};
+        }
+
+        // constructs a new element in place at the beginning
+        // returns a reference to the new element (i.e. front())
+        template <class... Args>
+            requires std::constructible_from<value_type, Args...>
+        constexpr reference
+        emplace_front(Args&&... args)
+            noexcept(noexcept(emplace(cbegin(), std::forward<Args>(args)...)))
+        {
+            emplace(cbegin(), std::forward<Args>(args)...);
+            return front();
+        }
+
+        // constructs a new element in place at the end
+        // returns a reference to the new element (i.e. back())
+        template <class... Args>
+            requires std::constructible_from<value_type, Args...>
+        constexpr reference
+        emplace_back(Args&&... args)
+            noexcept(noexcept(emplace(cend(), std::forward<Args>(args)...)))
+        {
+            emplace(cend(), std::forward<Args>(args)...);
+            return back();
+        }
+
+        // removes the element at pos
+        // returns ++pos if pos is not end(), otherwise end()
+        constexpr iterator
+        erase(const_iterator pos) noexcept
+        {
+            // check end()
+            if (pos == cend()) { return end(); }
+
+            SBMR_ASSERT_CONSTEXPR(!empty());
+
+            // get node and setup ret
+            auto *node = const_cast<node_type *>(pos.m_node);
+            auto it_ret = iterator{const_cast<node_type *>((++pos).m_node)};
+
+            auto*& new_next = ((node == m_head) ? m_head : node->prev->next);
+            auto*& new_prev = ((node == m_tail) ? m_tail : node->next->prev);
+
+            new_next = node->next;
+            new_prev = node->prev;
+
+            delete_node(node);
+            --m_size;
+
+            return it_ret;
+        }
+
+        // removes the elements in the range [first, last)
+        constexpr iterator
+        erase(const_iterator first, const_iterator last) noexcept
+        {
+            auto _first = iterator{const_cast<node_type *>(first.m_node)};
+            auto _last  = iterator{const_cast<node_type *>(last.m_node)};
+
+            while (_first != _last) { _first = erase(_first); }
+
+            return _first;
+        }
+
+        constexpr void
+        clear() noexcept
+        {
+            erase(cbegin(), cend());
         }
     };
 
